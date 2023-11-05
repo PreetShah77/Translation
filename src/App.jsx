9023ed86edff28717cdb5d955a0d434be4600d0c
PreetShah77 <98581633+PreetShah77@users.noreply.github.com>
2023-11-05 11:40:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import TextTranslate from './TextTranslate';
 import AudioTranslate from './AudioTranslate';
 import PdfTranslate from './PdfTranslate';
+import VideoTranslate from './VideoTranslate'; // Import the VideoTranslate component
 
 import './App.css'; // Import your main CSS file for the App
 
@@ -15,7 +15,7 @@ function App() {
       <div className="dashboard">
         <nav className="nav-bar">
           <ul className="nav-list">
-          <li className="nav-item">
+            <li className="nav-item">
               <Link to="/">Home</Link>
             </li>
             <li className="nav-item">
@@ -27,15 +27,18 @@ function App() {
             <li className="nav-item">
               <Link to="/pdf-translate">PDF Translate</Link>
             </li>
-
+            <li className="nav-item">
+              <Link to="/video-translate">Video Translate</Link> {/* Add a link for Video Translate */}
+            </li>
           </ul>
         </nav>
 
         <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/text-translate" element={<TextTranslate />} />
           <Route path="/audio-translate" element={<AudioTranslate />} />
           <Route path="/pdf-translate" element={<PdfTranslate />} />
+          <Route path="/video-translate" element={<VideoTranslate />} /> {/* Add a route for Video Translate */}
         </Routes>
       </div>
     </Router>
